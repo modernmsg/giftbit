8fdfc97b7b60b1269ad2e174e611f38110857b13
audeemac <amvelasco@gmail.com>
2013-12-18 15:33:27 -0800

    more pending spec

--- spec/kiind_spec.rb
+++ spec/kiind_spec.rb
@@ -10,4 +10,50 @@ describe Kiind do
 	  	pending
 	  end
   end
+
+  describe "#marketplace" do
+    it "list all gifts" do
+      pending
+    end
+
+    it "list all gifts by vendor" do
+    	pending
+    end
+  end
+
+  describe "#regions" do
+  	it "list all regions" do
+  		pending
+  	end
+  end
+
+  describe "#vendors" do
+  	it "list all regions" do
+  		pending
+  	end
+  end
+
+  describe "#categories" do
+  	it "list all categories" do
+  		pending
+  	end
+  end
+
+  describe "#creategift" do
+  	it "create quote for gift" do
+  		pending
+  	end
+  end
+
+  describe "#sendgift" do
+  	it "sends the gift with ID" do
+  		pending
+  	end
+  end
+
+  describe "#deletegift" do
+  	it "deletes the gift with ID" do
+  		pending
+  	end
+  end
 end
